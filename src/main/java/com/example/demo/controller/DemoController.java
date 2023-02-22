15505d5f44a2e9717240229e8d59730a7603ad0c
zhanghuocheng <useremail@qq.com>
2023-02-22 22:58:47 +0800

    日志链路

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,23 +3,23 @@ package com.example.demo.controller;
 import com.example.demo.async.AsyncTest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-@RestController
+@Component
 @Slf4j
-public class DemoController {
+public class DemoController implements CommandLineRunner {
 
 
     @Autowired
     private AsyncTest asyncTest;
 
-    @GetMapping("/demo")
-    public String demo() {
+
+    @Override
+    public void run(String... args) throws Exception {
         log.info("test");
         for (int i = 0; i <10 ; i++) {
             asyncTest.test();
         }
-        return "hello";
     }
 }
